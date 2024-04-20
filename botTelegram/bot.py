225cfb1d0392d62c562874acba99445fa11030f3
Marco Olvera <marcolvera333@gmail.com>
2024-04-20 08:13:55 -0600

    mensajes personalizados

--- botTelegram/bot.py
+++ botTelegram/bot.py
@@ -5,23 +5,36 @@ recopilar_info = False
 #posibles_escenarios = {"1" : "3", "2": "3","3": "4", "4" : "5", "5" : "3", "6" : "4", "7" : "4"}
 posibles_escenarios = ["1","2","3","4","5","6","7"]
 informacion_incidencia = []
+preguntas_trafico = ["¿Cual es tu ubicacion actual?", "¿Describe la intensidad del trafico del 1 al 3?", "¿Cuanto tiempo llevas atorado en el trafico? (redondea a la hora mas cercana)"] 
+preguntas_accidente = ["¿Donde te encuentras?", "¿Que tan grave es el accidente del 1 al 3?"]
+
+
+preguntas = []
+preguntas = preguntas_trafico
+i = 0
+final = False
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     global recopilar_info
+    global i
     guardar_txt()
     informacion_incidencia.clear()
     recopilar_info = False
+    i = 0
 
 @bot.message_handler(func=lambda message : True)
 def message(message):
+    mensaje_respuesta = ""
+    global i
     global recopilar_info
+    global final
     if not recopilar_info: #si aun no recopila informacion verifica si ya ingreso un valor del 1 al 7
         bot.reply_to(message, """Hola en que te puedo ayudar: 
                     1- Trafico
                     2- Accidente
                     3- Gasolina
-                    4- 5 Asaltos
+                    4- Asaltos
                     5- Clima
                     6- Falla mecanica
                     7- Documentacion 
@@ -30,8 +43,31 @@ def message(message):
             if message.text == escenario:
                 recopilar_info = True
                 informacion_incidencia.append(message.text)
-    else:
+    else:# se ejecutara cuando el usuario empieze a reporta informacion relevante
         informacion_incidencia.append(message.text)
+        if not final:
+            bot.reply_to(message, preguntas[i])
+        if i<len(preguntas)-1: #solo se suma cuando no es la ultima
+            i = i +1
+        else:
+            final = True
+            
+        
+
+        """if message.text == "1":
+            bot.reply_to(message, "¿Donde te encuentras?")
+        if message.text == "2":
+            pass
+        if message.text == "3":
+            pass
+        if message.text == "4":
+            pass
+        if message.text == "5":
+            pass
+        if message.text == "6":
+            pass
+        if message.text == "7":
+            pass"""
 
 def guardar_txt():
     with open('informacion.txt', 'w') as file:
--- botTelegram/views.py
+++ botTelegram/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from models import Trafico, Vehiculo, Operador
+from models import Trafico, Vehiculo, Operador, Accidentes, Gasolina, Asaltos, Clima, FallaMecanica, Documentacion
 from datetime import datetime
 # Create your views here.
 
@@ -11,7 +11,35 @@ def guardar_datos(request):
     if(int(contenido[0]) == 1):
         incidencia = Trafico(Ubicacion = contenido[1], IntensidadTrafico = contenido[2], Fecha = datetime.now())
         incidencia.save()
- 
+    
+    if int(contenido[0] == 2):
+        incidencia = Accidentes(Ubicacion = contenido[1], GravedadAccidente = contenido[2], Fecha = datetime.now())
+        incidencia.save()
+    
+    if int(contenido[0] == 3):
+        incidencia = Gasolina(PorcentajeGasolina = contenido[1], DineroDisponible = contenido[2], Ubicacion = contenido[3])
+        incidencia.save()
+
+    if int(contenido[0] == 4):
+       incidencia = Asaltos(Ubicacion = contenido[1], Fecha = datetime.now(), PerdidasEconomicas = contenido[3])
+       incidencia.save()
+
+    if int(contenido[0] == 5):
+       incidencia = Clima(Ubicacion = contenido[1], TipoClima = contenido[2], Fecha = datetime.now())
+       incidencia.save()
+
+    if int(contenido[0] == 6):
+         incidencia = FallaMecanica(Ubicacion = contenido[1], Fecha = datetime.now(), Falla = contenido[3])
+         incidencia.save()
+
+    if int(contenido[0] == 7):
+         incidencia = Documentacion(Fecha = datetime.now(), TipoDocumentacion = contenido[2])
+         incidencia.save()
+
+def respuestas():
+    contenido = leer_archivo().split('\n')
+    pass
+
 def leer_archivo():
     content = open('informacion.txt', 'r')
     contenido = content.read()
